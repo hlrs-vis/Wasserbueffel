114ad801908ae3fba4276abf3a099631029102f7
Uwe Woessner <woessner@hlrs.de>
2024-11-06 09:19:11 +0100

    new baeume page

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
+import ImageLayer from 'ol/layer/Image';
+import Static from 'ol/source/ImageStatic';
+import Projection from 'ol/proj/Projection'
 import Feature from 'ol/Feature';
 import { LineString, Point, Polygon } from 'ol/geom';
 import View from 'ol/View';
@@ -21,15 +24,23 @@ import Event from 'ol/events/Event';
 
 import { Vector as VectorLayer } from 'ol/layer';
 import { TileJSON, Vector as VectorSource } from 'ol/source';
-import { Fill, Icon, Stroke, Style } from 'ol/style';
+import { Circle, Text, Fill, Icon, Stroke, Style } from 'ol/style';
 
 
+var Bdates = [
+  '22.05.2020',
+  '11.06.2020',
+  '12.06.2020',
+];
 var dates = [
   '2019_03',
   '2019_05',
   '2020_03',
   '2020_04',
   '2020_05',
+  '2020_06',
+  '2020_09',
+  '2024_07',
 ];
 var renderGridCheckbox = document.getElementById('render-grid');
 var renderGridRotCheckbox = document.getElementById('render-grid-rot');
@@ -56,9 +67,101 @@ const vSource = new VectorSource({
 
 //undoRedo = new UndoRedo(vSource);	
 
+var image = new Circle({
+        radius: 5,
+        fill: null,
+        stroke: new Stroke({color: 'red', width: 1})
+      });
+var styles = {
+        'Point': new Style({
+          image: image
+        }),
+        'LineString': new Style({
+          stroke: new Stroke({
+            color: 'green',
+            width: 1
+          })
+        }),
+        'MultiLineString': new Style({
+          stroke: new Stroke({
+            color: 'green',
+            width: 1
+          })
+        }),
+        'MultiPoint': new Style({
+          image: image
+        }),
+        'MultiPolygon': new Style({
+          stroke: new Stroke({
+            color: 'yellow',
+            width: 1
+          }),
+          fill: new Fill({
+            color: 'rgba(255, 255, 0, 0.1)'
+          })
+        }),
+        'Polygon': new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            lineDash: [4],
+            width: 3
+          }),
+          fill: new Fill({
+            color: 'rgba(0, 0, 255, 0.1)'
+          })
+        }),
+        'GeometryCollection': new Style({
+          stroke: new Stroke({
+            color: 'magenta',
+            width: 2
+          }),
+          fill: new Fill({
+            color: 'magenta'
+          }),
+          image: new Circle({
+            radius: 10,
+            fill: null,
+            stroke: new Stroke({
+              color: 'magenta'
+            })
+          })
+        }),
+        'Circle': new Style({
+          stroke: new Stroke({
+            color: 'red',
+            width: 2
+          }),
+          fill: new Fill({
+            color: 'rgba(255,0,0,0.2)'
+          })
+        })
+      };
+
+
+var styleFunction = function(feature) {
+
+        var text = new Style({
+            text :new Text({
+                text: feature.getProperties().name,
+                font: '12px Calibri,sans-serif',
+                weight:'Bold',
+                fill: new Fill({ color: '#000' }),
+                stroke: new Stroke({
+                    color: '#D3D3D3', width: 2
+                }),
+                offsetX: 0,
+                offsetY: 0,
+                rotation: 0
+            })
+        });
+        return [styles[feature.getGeometry().getType()],text];
+      };
+
+
 const vLayer = new VectorLayer({
   source: vSource,
-  style: new Style({
+  style: styleFunction
+/*  style: new Style({
     stroke: new Stroke({
       width: 2,
       color: 'white'
@@ -66,9 +169,8 @@ const vLayer = new VectorLayer({
     fill: new Fill({
       color: [255,255,255,0]
     })
-  })
+  A})*/
 });
-
 function scaleControl() {
   if (scaleType === 'scaleline') {
     control = new ScaleLine({
@@ -87,6 +189,8 @@ function scaleControl() {
 }
 
 var layers = [];
+var flightLayers = [];
+var Blayers = [];
 var gridLines = [];
 var gridRotLines = [];
 var i, ii;
@@ -160,8 +264,7 @@ for (i = 0; i < (800/15.6); i++) {
 }
 
 for (i = 0, ii = dates.length; i < ii; ++i) {
-  layers.push(
-    new TileLayer({
+   var fl= new TileLayer({
       source: new TileXYZ({
         url: 'https://wasserbueffel.hlrs.de/' + dates[i] + '/code/odm_orthophoto/odm_orthophoto/{z}/{x}/{-y}.png',
         type: 'png',
@@ -169,8 +272,32 @@ for (i = 0, ii = dates.length; i < ii; ++i) {
       }),
       visible: false,
       preload: Infinity,
+    });
+  layers.push(fl);
+  flightLayers.push(fl);
+}
+
+pos = transform(origin, 'EPSG:4326', 'EPSG:3857');
+pos[0] += -263;
+pos[1] += -703;
+
+var extent = [pos[0]+0, pos[1]+0, pos[0]+3000, pos[1]+3000];
+//var extent = [0, 0, 512, 512];
+var projection = new Projection({
+  code: 'static-image',
+  units: 'pixels',
+  extent: extent
+});
+
+for (i = 0, ii = Bdates.length; i < ii; ++i) {
+Blayers.push(new ImageLayer({
+      source: new Static({
+        url: 'https://wasserbueffel.hlrs.de/fauna/WBAvifauna_'+Bdates[i]+'NoBG.svg',
+        projection: 'EPSG:3857',
+        imageExtent: extent
+      })
     })
-  );
+);
 }
 var gridLayer =
   new VectorLayer({
@@ -205,6 +332,11 @@ var gridRotLayer =
 layers.push(gridLayer);
 layers.push(gridRotLayer);
 layers.push(vLayer);
+for (i = 0, ii = Bdates.length; i < ii; ++i) {
+layers.push(Blayers[i]);
+Blayers[i].setVisible(false);
+}
+Blayers[0].setVisible(true);
 
 
 var map = new Map({
@@ -304,9 +436,21 @@ vSource.on('change', function() {
 var select = document.getElementById('imageDate');
 function onChange() {
   var date = select.value;
-  for (var i = 0, ii = layers.length - 5; i < ii; ++i) {
-    layers[i + 2].setVisible(dates[i] === date);
+    console.log(date);
+  for (var i = 0, ii = dates.length ; i < ii; ++i) {
+    flightLayers[i].setVisible(dates[i] === date);
+    console.log(dates[i] === date);
+    console.log(dates[i]);
   }
 }
 select.addEventListener('change', onChange);
 onChange();
+var birdSelect = document.getElementById('birdsDate');
+function BonChange() {
+  var date = birdSelect.value;
+  for (var i = 0, ii = Blayers.length; i < ii; ++i) {
+    Blayers[i].setVisible(Bdates[i] === date);
+  }
+}
+birdSelect.addEventListener('change', BonChange);
+BonChange();
